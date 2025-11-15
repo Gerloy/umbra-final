9600bbe1fe1db681996fe0bc44e7300f1f2968b3
Gerloy <gersilro@hotmail.com>
2025-11-15 03:33:02 -0300

    Audio terminado
    Queda solo cambiar el codigo de la Raspberry

--- Assets/Scripts/OSC/Recibidor.cs
+++ Assets/Scripts/OSC/Recibidor.cs
@@ -6,6 +6,10 @@ public class Recibidor : MonoBehaviour
 {
     [SerializeField] OSC osc;
     [SerializeField] cant cants;
+    [SerializeField] AudioSource snd_false;
+    [SerializeField] AudioSource snd_true;
+    int termino = 0;
+    int p_termino = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +19,14 @@ public class Recibidor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (termino == 1 && p_termino != termino){
+            snd_false.Stop();
+            snd_true.Play();
+        }else if (termino == 0 && p_termino != termino){
+            snd_false.Play();
+            snd_true.Stop();
+        }
+        p_termino = termino;
     }
 
     void Recibe(OscMessage mensaje)
@@ -25,8 +36,9 @@ public class Recibidor : MonoBehaviour
         //if (mensaje.address == "/deportistas") { cants.setDeportistas(mensaje.GetInt(0),mensaje.GetInt(1)); }
         //if (mensaje.address == "/cocineros") { cants.setCocineros(mensaje.GetInt(0),mensaje.GetInt(1)); }
 
-        if (mensaje.address == "/artistas") { cants.setArtistas(mensaje.GetInt(0));}
-        if (mensaje.address == "/deportistas") { cants.setDeportistas(mensaje.GetInt(0));}
-        if (mensaje.address == "/cocineros") { cants.setCocineros(mensaje.GetInt(0)); Debug.Log("Llego mensaje de cocineros");}
+        //if (mensaje.address == "/artistas") { cants.setArtistas(mensaje.GetInt(0));}
+        //if (mensaje.address == "/deportistas") { cants.setDeportistas(mensaje.GetInt(0));}
+        //if (mensaje.address == "/cocineros") { cants.setCocineros(mensaje.GetInt(0)); Debug.Log("Llego mensaje de cocineros");}
+        if (mensaje.address == "/termino"){termino = mensaje.GetInt(0);}
     }
 }
